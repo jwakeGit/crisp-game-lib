23e1ecf64d23526eae4db4a45d02565cc92de409
Stryker Buffington <sbuffing@ucsc.edu>
2021-10-08 16:32:11 -0700

    added FSM framework

--- docs/ringout/main.js
+++ docs/ringout/main.js
@@ -98,12 +98,8 @@ let rightGoalPos;
 
 function update() {
   if (!ticks) {
-    px = 30;
-    ex = 49;
-    pState = "idle";
-    eState = "idle";
-    leftGoalPos = vec(10, 32);
-    rightGoalPos = vec(68, 32);
+    Initialize();
+    
   }
   color("black");
   rect(10, 36, 60, 20);
@@ -119,3 +115,141 @@ function update() {
   }
   
 }
+
+function Initialize(){
+  px = 30;
+  ex = 49;
+  pState = "idle";
+  eState = "idle";
+  leftGoalPos = vec(10, 32);
+  rightGoalPos = vec(68, 32);
+
+
+  let state = machine.value
+  console.log(`current state: ${state}`)
+  state = machine.transition(state, 'startpunch')
+  console.log(`current state: ${state}`)
+  state = machine.transition(state, 'stoppunch')
+  console.log(`current state: ${state}`)
+}
+
+
+function createMachine(stateMachineDefinition) {
+  const machine = {
+    value: stateMachineDefinition.initialState,
+    transition(currentState, event) {
+      const currentStateDefinition = stateMachineDefinition[currentState]
+      const destinationTransition = currentStateDefinition.transitions[event]
+      if (!destinationTransition) {
+        return
+      }
+      const destinationState = destinationTransition.target
+      const destinationStateDefinition =
+        stateMachineDefinition[destinationState]
+
+      destinationTransition.action()
+      currentStateDefinition.actions.onExit()
+      destinationStateDefinition.actions.onEnter()
+
+      machine.value = destinationState
+
+      return machine.value
+    },
+  }
+  return machine
+}
+
+const machine = createMachine({
+  initialState: 'idle',
+  idle: {
+    actions: {
+      onEnter() {
+        console.log('idle: onEnter')
+      },
+      onExit() {
+        console.log('idle: onExit')
+      },
+    },
+    transitions: {
+      startpunch: {
+        target: 'punch',
+        action() {
+          console.log('transition action for "startpunch" from idle')
+        },
+      },
+      startblock: {
+        target: 'block',
+        action() {
+          console.log('transition action for "startblock" from idle')
+        },
+      },
+      startflinch: {
+        target: 'flinch',
+        action() {
+          console.log('transition action for "startflinch" from idle')
+        },
+      },
+    },
+  },
+  punch: {
+    actions: {
+      onEnter() {
+        console.log('punch: onEnter')
+      },
+      onExit() {
+        console.log('punch: onExit')
+      },
+    },
+    transitions: {
+      stoppunch: {
+        target: 'idle',
+        action() {
+          console.log('transition action for "stoppunch" from punch')
+        },
+      },
+      isblocked: {
+        target: 'flinch',
+        action() {
+          console.log('transition action for "isblocked" from punch')
+        },
+      },
+    },
+  },
+  block: {
+    actions: {
+      onEnter() {
+        console.log('block: onEnter')
+      },
+      onExit() {
+        console.log('block: onExit')
+      },
+    },
+    transitions: {
+      stopblock: {
+        target: 'idle',
+        action() {
+          console.log('transition action for "stopblock" from block')
+        },
+      },
+    },
+  },
+  flinch: {
+    actions: {
+      onEnter() {
+        console.log('flinch: onEnter')
+      },
+      onExit() {
+        console.log('flinch: onExit')
+      },
+    },
+    transitions: {
+      stopflinch: {
+        target: 'idle',
+        action() {
+          console.log('transition action for "stopflinch" from flinch')
+        },
+      },
+    },
+  },
+})
+
